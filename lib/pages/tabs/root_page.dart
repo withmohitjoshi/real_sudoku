4af23dd1ab94b8bcc0275b2277e953570101d845
Mohit <mohitjoshi.vc@cleartrip.com>
2025-05-21 18:30:18 +0530

    changes

---
+++ lib/pages/tabs/root_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:sudoku/pages/tabs/home/home_page.dart';
+import 'package:sudoku/pages/tabs/profile/profile_page.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomePage(),
+    const ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: const Color.fromARGB(20, 0, 0, 0),
+              blurRadius: 10,
+              offset: const Offset(0, -2),
+            ),
+          ],
+          border: Border(
+            top: BorderSide(
+              color: const Color.fromARGB(50, 96, 125, 139),
+              width: 1,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          selectedFontSize: 12.0,
+          unselectedFontSize: 12.0,
+          iconSize: 24.0,
+          selectedIconTheme: const IconThemeData(size: 24.0),
+          unselectedIconTheme: const IconThemeData(size: 24.0),
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+        ),
+      ),
+    );
+  }
+}
